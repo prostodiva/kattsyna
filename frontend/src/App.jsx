5dd15c930fb7336aa0bb8b30c08dd715559e3401
prostodiva <rkattsyna@mail.ru>
2025-04-28 18:16:57 -0700

    added the home component to the homePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
+import  HomePage from './pages/HomePage';
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/home" replace />} />
+            <Route path="/home" element={<HomePage />} />
+        </Routes>
+    )
+}
+
 
 const App = () => {
     return (
         <>
-
+            <AppRoutes />
         </>
     )
 };
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Home from '../components/Home'
+
+const HomePage = () => {
+    return (
+        <div>
+            <main>
+                <Home />
+            </main>
+        </div>
+
+    )
+}
+
+export default HomePage;
