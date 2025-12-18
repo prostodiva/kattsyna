5bb5ef870ea444da8b4cedcafb58649132eabb54
prostodiva <rkattsyna@mail.ru>
2025-12-18 11:29:03 -0800

    Add: export nfl

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -5,6 +5,7 @@ import crm from "./crm.png";
 import nft from "./nft.png";
 import briefs from "../assets/dev-briefs.png";
 import chatbot from "../assets/chatbot.png";
+import nfl from "../assets/nfl.png";
 
-export {  club, crm, nft, resumePDF, briefs, chatbot };
+export {  club, crm, nft, resumePDF, briefs, chatbot, nfl };
 
